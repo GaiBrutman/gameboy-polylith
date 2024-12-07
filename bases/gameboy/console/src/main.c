92f48238d519c8828c32c293676d9c7bcc89eb38
GaiBrutman <gai.brutman@gmail.com>
2024-12-08 01:58:39 +0200

    Add Gameboy start screen ASCII art to main console output

--- bases/gameboy/console/src/main.c
+++ bases/gameboy/console/src/main.c
@@ -6,6 +6,13 @@
 
 #define NUM_GAMES 1
 
+#define GAMEBOY_START_SCREEN "  ________                     ___.                 \n" \
+                             " /  _____/_____    _____   ____\\_ |__   ____ ___.__.\n" \
+                             "/   \\  ___\\__  \\  /     \\_/ __ \\| __ \\ /  _ <   |  |\n" \
+                             "\\    \\_\\  \\/ __ \\|  Y Y  \\  ___/| \\_\\ (  <_> )___  |\n" \
+                             " \\______  (____  /__|_|  /\\___  >___  /\\____// ____|\n" \
+                             "    \\/     \\/      \\/     \\/    \\/       \\/\n"
+
 static game_t sg_games[NUM_GAMES] = {0};
 
 static void initialize_games(void)
@@ -18,13 +25,15 @@ int main(void)
     ui_screen_t screen;
 
     initialize_games();
-    printf("Welcome to the Gameboy console!\n");
+    printf(GAMEBOY_START_SCREEN "\n");
+    printf("Welcome to the Gameboy console!\n\n");
 
     printf("Available games:\n");
     for (int i = 0; i < NUM_GAMES; i++)
     {
         printf("%d. %s - %s\n", i + 1, sg_games[i].name, sg_games[i].description);
     }
+    printf("\n");
 
     int game_choice = 0;
     printf("Enter the number of the game you would like to play: ");
